6912c33fcb954552dd84efe17f7f80abb933970d
student <faithnyamburawamui@gmail.com>
2024-10-02 20:05:33 +0300

    added code

--- app/src/main/java/com/example/multimedia/ui/HomeActivity.kt
+++ app/src/main/java/com/example/multimedia/ui/HomeActivity.kt
@@ -5,17 +5,49 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.multimedia.R
+import com.example.multimedia.databinding.ActivityHomeBinding
+import com.example.multimedia.databinding.ActivityMainBinding
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding=ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.bnvHome.setOnItemSelectedListener {menuItem->
+            when(menuItem.itemId){
+                R.id.home->{
+                    loadFragment(HomeFragment())
+                }
+                R.id.games->{
+                   loadFragment(GamesFragment())
+                }
+                R.id.profile->{
+                    val profileFragment = ProfileFragment()
+                    loadFragment(profileFragment)
+                }
+                R.id.settinge->{
+                   loadFragment(SettingFragment())
+                }
+
+                else->{
+                    loadFragment(HomeFragment())
+                }
+            }
         }
+    }
+
+    fun loadFragment(fragment: Fragment):Boolean{
+        val fragmentTransaction=supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fcvHome, fragment)
+        fragmentTransaction.commit()
+        return true
     }
 }
